65b406c1f7ddedef2c471f6f88ed850699b35961
muh-osman <mr.muhammed.osman@gmail.com>
2025-01-10 11:47:22 +0300

    first commit

---
+++ wp-content/themes/cashif_theme/assets/js/config357c.js
@@ -0,0 +1,7 @@
+const BACK_END_API = "payment-system/back-end/public/index.html";
+// const BACK_END_API = "http://localhost:8000";
+const FETCH_PRICES_API = "https://cashif.online/back-end/public";
+
+
+// Moyassar
+const PUBLISHABLE_API_KEY = "pk_live_PFUhf3sps2imKbuTt9cW87PQEmsQQbeoxm2xpNw9";
